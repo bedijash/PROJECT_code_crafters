c2f06278d28e082b576100bda19953624ed65638
Jashanpreet singh <157813015+bedijash@users.noreply.github.com>
2024-02-02 08:48:36 -0500

    Update Student.java

--- src/studentprint/Student.java
+++ src/studentprint/Student.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package studentprint;
-
+/**2 feb 2024
 /** model the student entity 
  *
  * @author kumar
